efc9432885819763ee4aba681697a78320d3d06f
Carolista <cjones.renaissancewoman@gmail.com>
2023-02-20 16:48:19 -0600

    removed TODO from GalleryController

--- art-gallery/src/main/java/com/launchcode/artgallery/controllers/GalleryController.java
+++ art-gallery/src/main/java/com/launchcode/artgallery/controllers/GalleryController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GalleryController {
 
-    // TODO: Create and use a template called index.html
-    // Remember to remove @ResponseBody
     // Corresponds to http://localhost:8080
     @GetMapping("/")
     public String displayHomePage(Model model) {
