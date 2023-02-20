6f3c699f7b4977ddb4be822411131d799f1f0f0d
Carolista <cjones.renaissancewoman@gmail.com>
2023-02-20 16:38:48 -0600

    removed TODOs from solution

--- art-gallery/src/main/java/com/launchcode/artgallery/controllers/CollectionController.java
+++ art-gallery/src/main/java/com/launchcode/artgallery/controllers/CollectionController.java
@@ -13,8 +13,6 @@ public class CollectionController {
 
     private static final ArrayList<String> artCollection = new ArrayList<>();
 
-    // TODO: Convert this to use a template, collection.html
-    // TODO: Pass list, artCollection, to template
     // Corresponds to http://localhost:8080/collection
     @GetMapping("")
     public String displayCollectionPage(Model model) {
@@ -23,7 +21,6 @@ public class CollectionController {
         return "collection/index";
     }
 
-    // TODO: Convert this to use a template, add-art-form.html
     // Corresponds to http://localhost:8080/collection/add
     @GetMapping("/add")
     public String displayAddArtForm() {
@@ -31,9 +28,6 @@ public class CollectionController {
         return "collection/add-art-form";
     }
 
-    // TODO: Create a POST handler for /add
-    // Should add name of artwork to collection list
-    // Then should redirect to /collection
     @PostMapping("/add")
     public String processAddArtForm(@RequestParam String artwork) {
         System.out.println("\n*** POST request submitted to add " + artwork + " to collection");
