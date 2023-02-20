172fe7d745f5c4e504518a44eb746f0a997534f5
Carolista <cjones.renaissancewoman@gmail.com>
2023-02-19 21:26:25 -0600

    added starter code to main for class 10 lecture

---
+++ art-gallery/src/test/java/com/launchcode/artgallery/ArtGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.artgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
